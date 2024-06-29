0f4f262d237bac718ca4c179e967b95bc967f56a
Joseph <josephpajuelo7@gmail.com>
2024-06-30 00:58:05 +0200

    Interfaz y nave desplazados

---
+++ Space/src/graphics/Loader.java
@@ -0,0 +1,21 @@
+package graphics;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Loader {
+
+    public static BufferedImage ImageLoader(String path)
+    {
+        try
+        {
+            return ImageIO.read(Loader.class.getResource(path));
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
